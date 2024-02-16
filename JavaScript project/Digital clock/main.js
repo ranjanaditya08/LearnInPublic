d77172746bf1bae0335400560e10af89b004bf93
Aditya Ranjan <adityaranjann29@gmail.com>
2024-02-16 13:43:54 +0530

    completed digital clock project

--- JavaScript project/Digital clock/main.js
+++ JavaScript project/Digital clock/main.js
@@ -1,2 +1,35 @@
-const time = document.querySelector('.time');
-const date = document.querySelector('.date');
+const time = document.querySelector(".time");
+const updateDate = document.querySelector(".date");
+
+setInterval(function () {
+  let date = new Date();
+  time.innerHTML = `${date.toLocaleTimeString()}`;
+}, 1000);
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let date = new Date();
+let newDtae = `${days[date.getDay()]}, ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+updateDate.innerHTML = `${newDtae}`;
