04c8c9365ccd628062922ce912bca724b82d0928
Aditya Ranjan <adityaranjann29@gmail.com>
2024-03-19 13:01:38 +0530

    completed quiz app project

--- JavaScript project/quiz app/script.js
+++ JavaScript project/quiz app/script.js
@@ -45,19 +45,23 @@ const quizData = [
   },
 ];
 
+const quiz = document.getElementById("quiz");
 const questionEl = document.getElementById("question");
-
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
+const answerEls = document.querySelectorAll(".answer");
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
+  deselectRadioBtn();
+
   const currentQuizData = quizData[currentQuiz];
 
   questionEl.innerHTML = currentQuizData.question;
@@ -68,21 +72,35 @@ function loadQuiz() {
 }
 
 function getSelectedRadioBtn() {
-  const answerEl = document.querySelectorAll(".answer");
-  console.log(answerEl);
-  answerEl.forEach((answer) => {
-    if (answer.checked) {
-      answer = answer.id;
+  let answer;
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
     }
+  });
+  return answer;
+}
+
+function deselectRadioBtn() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
   });
 }
 
 submitBtn.addEventListener("click", () => {
-  getSelectedRadioBtn();
-  currentQuiz++;
-  if (currentQuiz < quizData.length) {
-    loadQuiz();
-  } else {
-    alert("You finished,Get yourself a Orange Lemonade");
+  const answer = getSelectedRadioBtn();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `<h2>You answered correctly  ${score}/${quizData.length} questions.</h2>
+                        <button onclick="location.reload()">Reload</button>`;
+    }
   }
 });
